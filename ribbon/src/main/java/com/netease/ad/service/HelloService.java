88e1bfab797424f48cad3a84e085f83d05a6877f
bjzhuyucheng <bjzhuyucheng@corp.netease.com>
2017-04-27 17:01:42 +0800

    1. 添加ribbon hystrix支持

--- ribbon/src/main/java/com/netease/ad/service/HelloService.java
+++ ribbon/src/main/java/com/netease/ad/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.netease.ad.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,13 @@ public class HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public String hiService (String name) {
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String hiService(String name) {
         log.info("hi service {}", name);
         return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi, " + name + ", sorry, error!";
     }
 }
