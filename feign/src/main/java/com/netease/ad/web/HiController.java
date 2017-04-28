fa26bf333f56988bfae58b20e3a7bd6464f5aa6b
bjzhuyucheng <bjzhuyucheng@corp.netease.com>
2017-04-28 10:14:54 +0800

    添加zuul路由支持

--- feign/src/main/java/com/netease/ad/web/HiController.java
+++ feign/src/main/java/com/netease/ad/web/HiController.java
@@ -1,6 +1,7 @@
 package com.netease.ad.web;
 
 import com.netease.ad.service.SchedualServiceHi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by bjzhuyucheng on 2017/4/27.
  */
 @RestController
+@Slf4j
 public class HiController {
     @Autowired
     private SchedualServiceHi schedualServiceHi;
 
     @GetMapping(value = "/hello")
     public String sayHi(@RequestParam String name) {
+        log.info("hello {}", name);
         return schedualServiceHi.sayHiFromClientOne(name);
     }
 }
--- ribbon/src/main/java/com/netease/ad/web/HelloController.java
+++ ribbon/src/main/java/com/netease/ad/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.netease.ad.web;
 
 import com.netease.ad.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by bjzhuyucheng on 2017/4/27.
  */
 @RestController
-public class HelloControler {
+@Slf4j
+public class HelloController {
     @Autowired
     private HelloService helloService;
 
     @GetMapping(value = "/hello")
     public String hi(@RequestParam String name) {
+        log.info("hello {}", name);
         return helloService.hiService(name);
     }
 
