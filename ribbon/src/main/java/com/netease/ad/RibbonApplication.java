5fbe21929cca050c549dd6864255bcc32f0e0a55
bjzhuyucheng <bjzhuyucheng@corp.netease.com>
2017-04-28 09:49:40 +0800

    bugfix

--- ribbon/src/main/java/com/netease/ad/RibbonApplication.java
+++ ribbon/src/main/java/com/netease/ad/RibbonApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableHystrix
 @EnableDiscoveryClient
 @SpringBootApplication
+//@EnableHystrixDashboard
 public class RibbonApplication {
 
     public static void main(String[] args) {
